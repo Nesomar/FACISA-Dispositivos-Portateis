b830f9728e78038d2d763ff41424d1e33b26232e
Nesomar <nesomar.ramalho@gmail.com>
2017-12-09 16:33:34 -0300

    atualização firebase

--- ProjetoCRUD/src/environments/environment.ts
+++ ProjetoCRUD/src/environments/environment.ts
@@ -1,10 +1,10 @@
  // Initialize Firebase
 export const firebaseConfig = {
-    apiKey: "AIzaSyCpgTAfoRzD6oCefRs0KnmsvloLXSAHJow",
-    authDomain: "projetocrud-bf3f0.firebaseapp.com",
-    databaseURL: "https://projetocrud-bf3f0.firebaseio.com",
-    projectId: "projetocrud-bf3f0",
-    storageBucket: "projetocrud-bf3f0.appspot.com",
-    messagingSenderId: "914969411004"
+    apiKey: "your-credentials",
+    authDomain: "your-credentials",
+    databaseURL: "your-credentials",
+    projectId: "your-credentials",
+    storageBucket: "your-credentials",
+    messagingSenderId: "your-credentials"
   };
   
